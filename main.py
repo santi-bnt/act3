22aa745bdf6632e6f252e20c3ff5150917867665
Santiago Benavent <A01646121@tec.mx>
2025-10-29 12:57:17 -0600

    Add functions to detect different types of apples

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ pera(img2)
 
 img3 = 'pera_3.jpg'
 pera(img3)
+
+manzana_n = "manzana.jpg"
+detectar_manzana(manzana_n)
+
+manzana_np = "manzana_not_fresh.jpg"
+detectar_manzana(manzana_p)
+
+manzana_p = "manzana_podrida.jpg"
+detectar_manzana(manzana_np)
